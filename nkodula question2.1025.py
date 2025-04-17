99ad9771df37321f01c93fcd2373c5ea8d9ac6da
nkodula <kodulanicola@gmail.com>
2025-04-17 15:04:21 +0300

    Add files via upload

---
+++ nkodula question2.1025.py
@@ -0,0 +1,10 @@
+#CREATE A BANKING SYSTEM
+__init__(self, account number,balance=0)
+__init__(self, 6715738932, balance=0)
+let deposit be 5000
+print (deposit)
+if withdraw(2000)
+print(withdraw)
+deposit()
+withdraw()
+print(final balance)
---
+++ nkodula.1025 end sem.py
@@ -0,0 +1,13 @@
+#CREATE A PAYROLL THAT CALCULATES EMPLOYEE SALARIES
+calculate- salaryhourly 
+hours_worked
+tax_rate(0.15)
+if x(7):
+    
+  if y(24):
+print (x*y)
+
+  if tax_deduction (15%)
+print(x*y)/15%
+return netsalary after tax
+print(total)
